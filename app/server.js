7b9dbda5185d7cc3eae83d3c64707c99d9576869
German Rodriguez <german.at.work@gmail.com>
2018-11-03 02:21:09 -0300

    Add logging for unexpeced events and include query parser

--- app/server.js
+++ app/server.js
@@ -5,17 +5,28 @@ const mongoose = require('mongoose');
 
 const config = require('./config/todo-service-config');
 const configureRoutes = require('./route/routes');
-const logger = require('./util/logger');
+const {logger, bunyanAdaptedLogger} = require('./util/logger');
 
 logger.info('Start todo-list server. Configuration: ' + JSON.stringify(config));
 
 let server = restify.createServer({
-    name: 'todo-list'
+    name: 'todo-list',
+    log: bunyanAdaptedLogger
 });
 
 server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.queryParser());
 
 configureRoutes(server);
+
+server.on('uncaughtException', function (req, res, route, err) {
+  logger.error('uncaughtException: %s - %s', err.name, err.message);
+});
+
+server.on('restifyError', function(req, res, err, callback) {
+  logger.debug('Error on server: %s - %s', err.name, err.message);
+  return callback();
+});
 
 mongoose.connect(config.db.url);
 mongoose.connection.on('connected', function(error) {
