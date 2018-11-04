a886e3939f97d3c21f4a9a1d84544687612c7fa8
German Rodriguez <german.at.work@gmail.com>
2018-11-04 11:07:03 -0300

    Use model constants and add test for patch

--- test/integration/todo-service.spec.js
+++ test/integration/todo-service.spec.js
@@ -5,6 +5,7 @@ const expect = require('chai').expect;
 const hippie = require('hippie');
 
 const errors = require('../../app/model/errors');
+const modelConstants = require('../../app/model/model-constants');
 const server = require('../../app/server');
 const {cleanUpDb, createList, createTodo} = require('./integration-test-utils');
 
@@ -19,6 +20,22 @@ function verifyTodoOnResponse(response) {
   validator.to.have.property('creation_date').that.is.a('number');
   validator.to.have.property('last_update_date').that.is.a('number');
   validator.not.to.have.property('due_date');
+  validator.to.have.property('state').to.be.oneOf(_.values(modelConstants.todoStates));
+}
+
+function verifyTodoOnUpdateResponse(response, expected) {
+  let validator = expect(JSON.parse(response.body));
+  validator.to.have.property('description').that.eql(expected.description);
+  validator.to.have.property('id').that.eql(expected.id);
+  validator.to.have.property('creation_date').to.be.eql(expected.creation_date);
+  validator.to.have.property('last_update_date').to.be.gte(expected.last_update_date);
+  validator.to.have.property('state').eql(expected.state);
+
+  if (_.includes(_.keys(expected), 'due_date')) {
+    validator.to.have.property('due_date').eql(expected.due_date);
+  } else {
+    validator.not.to.have.property('due_date');
+  }
 }
 
 
@@ -383,7 +400,7 @@ describe('/todo/api/lists/:listId/todos/:todoId', function () {
       expect(JSON.parse(response.body)).to.include(errors.TodoNotFound);      
     });
 
-    it('should succedd if ToDo does exist', async function() {
+    it('should succeed if ToDo does exist', async function() {
       let response = await hippie(server)
                               .json()
                               .get(`/todo/api/lists/${storedList.id}/todos/${todo.id}`)
@@ -392,6 +409,81 @@ describe('/todo/api/lists/:listId/todos/:todoId', function () {
 
       verifyTodoOnResponse(response);
       
+    });
+  });
+
+  describe('patch', function () {
+
+    it('should fail if list does not exist', async function () {
+      return hippie(server)
+            .json()
+            .patch('/todo/api/lists/24564545/todos/234435')
+            .send({
+              state: modelConstants.todoStates.Complete
+            })
+            .expectStatus(404)
+            .end();
+    });
+
+    it('should fail if list id is not a number', async function() {
+      let response = await hippie(server)
+                              .json()
+                              .patch('/todo/api/lists/NotValidListId/todos/1')
+                              .send({
+                                state: modelConstants.todoStates.Complete
+                              })
+                              .expectStatus(400)
+                              .end();
+      expect(JSON.parse(response.body)).to.include(errors.IdNotAcceptable);
+    });
+
+    it('should fail if ToDo id is invalid', async function() {
+      let response = await hippie(server)
+                              .json()
+                              .patch(`/todo/api/lists/${storedList.id}/todos/InvalidId`)
+                              .send({
+                                state: modelConstants.todoStates.Complete
+                              })
+                              .expectStatus(400)
+                              .end();
+
+      expect(JSON.parse(response.body)).to.include(errors.IdNotAcceptable);
+    });
+
+    it('should fail if ToDo does not exist', async function() {
+      let response = await hippie(server)
+                              .json()
+                              .get(`/todo/api/lists/${storedList.id}/todos/233445654`)
+                              .send({
+                                state: modelConstants.todoStates.Complete
+                              })
+                              .expectStatus(404)
+                              .end();
+      expect(JSON.parse(response.body)).to.include(errors.TodoNotFound);      
+    });
+
+    it('should partially update an existing ToDo', async function() {
+      let response = await hippie(server)
+                              .json()
+                              .patch(`/todo/api/lists/${storedList.id}/todos/${todo.id}`)
+                              .send({
+                                state: modelConstants.todoStates.Complete
+                              })
+                              .expectStatus(200)
+                              .end();
+
+      let expected = _.assign({}, todo);
+      expected.state = modelConstants.todoStates.Complete;
+      verifyTodoOnUpdateResponse(response, expected);
+
+      response = await hippie(server)
+                          .json()
+                          .get(`/todo/api/lists/${storedList.id}/todos/${todo.id}`)
+                          .send()
+                          .expectStatus(200)
+                          .end();
+
+      verifyTodoOnUpdateResponse(response, expected);
     });
   });
 });
