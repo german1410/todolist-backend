d9d638127ace376a2322c671db444981eadd7985
German Rodriguez <german.at.work@gmail.com>
2018-11-05 03:03:46 -0300

    Add config documentation

--- app/config/todo-service-config.js
+++ app/config/todo-service-config.js
@@ -1,10 +1,29 @@
 'use strict';
 
+/**
+ * ToDo List service configuration
+ * 
+ * @module config/todo-service-config 
+ */
+
 const _ = require('lodash');
 
 let develpmentEnv = process.env.NODE_ENV === 'development';
 let dbUrl = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/todoList';
 
+/**
+ * Service configuration contains the following informaiton:
+ * 
+ * @property {boolean} production           - Indicates if service is configured to run in production environmnets
+ * @property {boolean} development          - Indicates if service is configured to run in development environmnets
+ * @property {object}  service              - Provides information specific to the server application
+ * @property {number}  service.port         - Indicates the port on which the service will listent. Default port is 8080
+ * @property {object}  db                   - Configuration properties specific for the DB connection
+ * @property {string}  db.url               - Connection URL used to connect to the DB
+ * @property {boolean} db.auto_create       - Wether or not to create the DB schema automatically. Only enabled for development environments.
+ * @property {object}  search               - Properties specific for search operations
+ * @property {number}  search.default_imit  - Default limit to use to list of ToDos when no limit is provided
+ */
 let config = {
   production: !develpmentEnv,
   development: develpmentEnv,
